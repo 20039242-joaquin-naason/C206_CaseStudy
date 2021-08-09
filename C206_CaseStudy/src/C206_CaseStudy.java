18114c81c066bdcef18bb0e79035f942c2482701
20039242 <20039242@LAPTOP-V4GANM47>
2021-08-09 19:28:40 +0800

    Update C206_CaseStudy again

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -7,8 +7,6 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// test
-
 		ArrayList<Promotion> promotionList = new ArrayList<Promotion>();
 
 		promotionList.add(new Promotion("Spaghetti", 5.00, 3.50, 6));

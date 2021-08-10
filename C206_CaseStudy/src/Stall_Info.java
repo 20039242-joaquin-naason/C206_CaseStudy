ab8b2f918f9754320aecd1bae93d4abcf46a6aa4
20013890 <20013890@LAPTOP-L8PV2H6D>
2021-08-10 23:40:07 +0800

    create stall class

---
+++ C206_CaseStudy/src/Stall_Info.java
@@ -0,0 +1,12 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 20013890, 10 Aug 2021 11:35:12 pm
+ */
+
+public class Stall_Info {
+
+}

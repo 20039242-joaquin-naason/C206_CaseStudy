90de95673c3a92ba8bf4df0827cec877cfd7d10b
20039242 <20039242@LAPTOP-V4GANM47>
2021-08-11 10:33:49 +0800

    Update Stall_info class

---
+++ C206_CaseStudy/src/Stall_Info.java
@@ -0,0 +1,34 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 20013890, 10 Aug 2021 11:35:12 pm
+ */
+
+public class Stall_Info {
+	
+	private int stallId;
+	private String name;
+	private String operationdate;
+
+	public Stall_Info(int stallId, String name, String operationdate) {
+		this.stallId = stallId;
+		this.name = name;
+		this.operationdate = operationdate;
+	}
+
+	public int getStallId() {
+		return stallId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getOperationdate() {
+		return operationdate;
+	}
+	
+}

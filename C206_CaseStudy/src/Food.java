d96395df886deff1eb8bc55d780ec51225eb9507
20047899 <20047899@Eddy>
2021-08-11 12:06:26 +0800

    food item class

---
+++ C206_CaseStudy/src/Food.java
@@ -0,0 +1,53 @@
+/*
+ * I declare that this code was written by me. 
+ * I will not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: Eddy Goh
+ * Student ID: 20047899
+ * Class: W64E
+ * Date/Time created: Wednesday 11-08-2021 12:01
+ */
+
+public class Food {
+	private int id;
+	private String name;
+	private double price;
+	private boolean isAvailable;
+
+	public Food(int id, String name, double price, boolean isAvailable) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.isAvailable = isAvailable;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+}
